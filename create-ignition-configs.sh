db4a079622b26dbe4c43cbf9a20673fafcf87c6d
mrobson <mrobson@redhat.com>
2019-07-03 15:36:13 -0400

    update include playbook syntax + directory fix in create ignition

--- create-ignition-configs.sh
+++ create-ignition-configs.sh
@@ -5,5 +5,5 @@ mkdir installer
 cp openstack-upi-install-config.yml installer/install-config.yaml
 ./openshift-install --dir=./installer create ignition-configs
 mv ./installer/bootstrap.ign ./openstack-openshift-playbook/roles/loadbalancer/files/bootstrap.ign
-mv ./installer/master.ign ./openstack-openshift-playbook/roles/ocpinstances/files/master.ign
-mv ./installer/worker.ign ./openstack-openshift-playbook/roles/ocpinstances/files/worker.ign
+mv ./installer/master.ign ./openstack-openshift-playbook/roles/openshift/files/master.ign
+mv ./installer/worker.ign ./openstack-openshift-playbook/roles/openshift/files/worker.ign
